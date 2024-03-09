13523757b2f170194dc3270af018e8ed8ad87789
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-09 01:09:49 -0600

    log in screen layout finish

--- lib/presentation/screens/logIn/log_in_screen.dart
+++ lib/presentation/screens/logIn/log_in_screen.dart
@@ -6,9 +6,41 @@ class LogInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/images/log_in_img.png',
-      fit: BoxFit.cover,
+    final currentYear = DateTime.now().year;
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/log_in_img.png', fit: BoxFit.cover),
+                const SizedBox(height: 20),
+                const Text(
+                  'Welcome to the Amazing Shopping App!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Iniciar sesión'),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            'Developed By Jaime Tenorio © $currentYear',
+            style: const TextStyle(
+              fontSize: 13,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
