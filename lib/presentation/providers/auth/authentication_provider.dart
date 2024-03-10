8eba3241d763cc0c42f5be0d3c85192c401e212b
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-09 20:50:13 -0600

    Add authentication provider and repository providers

---
+++ lib/presentation/providers/auth/authentication_provider.dart
@@ -0,0 +1,31 @@
+import 'package:amazing_shopping/domain/entities/auth/user_response.dart';
+import 'package:amazing_shopping/presentation/providers/auth/authentication_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authenticationNotifierProvider =
+    StateNotifierProvider<AuthenticationNotifier, UserResponse?>((ref) {
+  final authNotifier = ref.watch(authenticationRepositoryProvider);
+  return AuthenticationNotifier(
+    logIn: authNotifier.logIn,
+    logOut: authNotifier.logOut,
+  );
+});
+
+typedef LogInCallback = Future<UserResponse?> Function();
+typedef LogOutCallback = Future<void> Function();
+
+class AuthenticationNotifier extends StateNotifier<UserResponse?> {
+  final LogInCallback logIn;
+  final LogOutCallback logOut;
+  AuthenticationNotifier({required this.logIn, required this.logOut})
+      : super(null);
+
+  Future<void> logInUser() async {
+    state = await logIn();
+  }
+
+  Future<void> logOutUser() async {
+    await logOut();
+    state = null;
+  }
+}
