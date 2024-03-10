c72623475de070aa11195f191cfd7e77be0ec11c
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-09 20:00:21 -0600

    logIn function implementation in AuthenticationRepositoryImpl using firebase google auth

---
+++ lib/infrastructure/repositories/auth/authentication_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:amazing_shopping/domain/entities/auth/user_response.dart';
+import 'package:amazing_shopping/domain/repositories/auth/authentication_repository.dart';
+import 'package:amazing_shopping/infrastructure/mappers/auth/user_mapper.dart';
+
+class AuthenticationRepositoryImpl extends AuthenticationRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  Future<UserResponse> logIn() async {
+    final googleAuthProvider = GoogleAuthProvider();
+    final userCredential = await _auth.signInWithProvider(googleAuthProvider);
+    return UserMapper.castToEntity(userCredential.user!);
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+}
