b9775fb1a48a3c3d15de5ad05c055f09cc6eb5a9
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-09 19:18:44 -0600

    creation of user info entity for loading data from firebase google auth

---
+++ lib/domain/entities/auth/user_info.dart
@@ -0,0 +1,13 @@
+class UserInfo {
+  final String id;
+  final String? email;
+  final String? name;
+  final String? photoUrl;
+
+  UserInfo({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.photoUrl,
+  });
+}
