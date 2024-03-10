2a282a9c730d1260cbed86ffa583e5acbf55e6f1
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-09 20:52:48 -0600

    add export file for providers

---
+++ lib/presentation/providers/providers.dart
@@ -0,0 +1,2 @@
+export 'package:amazing_shopping/presentation/providers/auth/authentication_provider.dart';
+export 'package:amazing_shopping/presentation/providers/auth/authentication_repository_provider.dart';
